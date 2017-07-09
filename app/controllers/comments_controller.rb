eff3407e14123e36c7ad6b5a7690345bedc656c0
shomy <sksks0607@gmail.com>
2017-07-09 22:28:14 +0900

    l

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,9 +12,9 @@ class CommentsController < ApplicationController
       if @comment.save
         format.js {render :index}
         unless @comment.blog.user_id == current_user.id
-        Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
-          message: 'あなたの作成したブログにコメントが付きました'
-        })
+          Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
+            message: 'あなたの作成したブログにコメントが付きました'
+          })
         end
         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
           unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count

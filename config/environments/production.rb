0006f923430ab5174c3376b251db92a284eff3ce
shomy <sksks0607@gmail.com>
2017-07-16 13:50:10 +0900

    SENDGRIDのメール送り先をHEROKUからAWSに
    herokuからAWSに書き換え

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
 
-config.action_mailer.default_url_options = { host: 'zikkendive.herokuapp.com' }
+# config.action_mailer.default_url_options = { host: 'zikkendive.herokuapp.com' }
+config.action_mailer.default_url_options = { host: 'http://52.199.1.245' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
